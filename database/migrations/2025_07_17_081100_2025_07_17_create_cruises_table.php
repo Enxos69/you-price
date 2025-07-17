16e2e609521788822592180c6397910b5f064219
Enxos69 <enxos@hotmail.it>
2025-07-17 10:56:12 +0200

    migration cruises

---
+++ database/migrations/2025_07_17_081100_2025_07_17_create_cruises_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('cruises', function (Blueprint $table) {
+            $table->id();
+            $table->string('ship');
+            $table->string('cruise');
+            $table->string('duration')->nullable();
+            $table->string('from')->nullable();
+            $table->string('to')->nullable();
+            $table->string('details')->nullable();
+            $table->string('line');
+            $table->unsignedTinyInteger('night')->nullable();
+            $table->date('partenza')->nullable();
+            $table->date('arrivo')->nullable();
+            $table->string('interior')->nullable();
+            $table->string('oceanview')->nullable();
+            $table->string('balcony')->nullable();
+            $table->string('minisuite')->nullable();
+            $table->string('suite')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('cruises');
+    }
+};
