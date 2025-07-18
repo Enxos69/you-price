d2300e2e806fcbee519539577e3e5e5a32efd8b2
Enxos69 <enxos@hotmail.it>
2025-07-18 13:28:16 +0200

    fix navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/partials/navbar.blade.php -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <a class="navbar-brand" href="/">
-        <img src="{{ config('app.asset_url') }}/img/logo.png" alt="Logo" style="height: 50px;">
+        <img src="{{ env('ASSET_URL') }}/img/logo.png" alt="Logo" style="height: 50px;">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
         aria-expanded="false" aria-label="Toggle navigation">
@@ -9,66 +9,63 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mx-auto">
-            <!-- Menu orizzontale -->
+            <!-- Menu orizzontale per ruolo -->
             @auth
-                <!-- Controllo semplice per admin: se l'utente ha ID 1 o email contiene admin -->
-                @if (Auth::user()->id == 1 || strpos(Auth::user()->email, 'admin') !== false)
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.index') }}">Dashboard Admin</a>
+
+                @if (Auth::user()->isAdmin())
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item" href="#">Dashboard</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('users.index') }}">Gestione Utenti</a>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item" href="{{ route('user.index') }}">Gestione Utenti</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('cruises.import.form') }}">Importa Crociere</a>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-item" href="{{ route('cruises.import.form') }}">Importa Crociere</a>
                     </li>
-                @else
+                @elseif(Auth::user()->isUser())
+                    <a class="dropdown-item" href="#">Profilo</a>
+                    <a class="dropdown-item" href="{{ route('richieste.index') }}">Le Mie Richieste</a>
+                    <a class="dropdown-item" href="#">Supporto</a>
+                @endif
+        </div>
+        </li>
+    @endauth
+    </ul>
+    <ul class="navbar-nav ml-auto">
+        @if (Route::has('login'))
+            @auth
+                <li class="nav-item w-100">
+                    <a class="nav-link" href="{{ url('/home') }}">
+                        <i class="fa-solid fa-house"></i> Home
+                    </a>
+                    <a class="nav-link ml-3" href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fa-solid fa-right-from-bracket"></i> Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            @else
+                <li class="nav-item">
+                    <a href="{{ route('crociere.index') }}" class="btn btn-primary ml-3">
+                        Fai la tua Ricerca!
+                    </a>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">
+                        <i class="fas fa-sign-in-alt"></i> Login
+                    </a>
+                </li>
+                @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('user.index') }}">Profilo</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('richieste.index') }}">Le Mie Richieste</a>
+                        <a class="nav-link" href="{{ route('register') }}">
+                            <i class="fas fa-user-plus"></i> Registrati
+                        </a>
                     </li>
                 @endif
+                </li>
             @endauth
-        </ul>
-        <ul class="navbar-nav ml-auto">
-            @if (Route::has('login'))
-                @auth
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/home') }}">
-                            <i class="fa-solid fa-house"></i> Home
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('logout') }}"
-                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            <i class="fa-solid fa-right-from-bracket"></i> Logout
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </li>
-                @else
-                    <li class="nav-item">
-                        <a href="{{ route('crociere.index') }}" class="btn btn-primary ml-3">
-                            Fai la tua Ricerca!
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">
-                            <i class="fas fa-sign-in-alt"></i> Login
-                        </a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">
-                                <i class="fas fa-user-plus"></i> Registrati
-                            </a>
-                        </li>
-                    @endif
-                @endauth
-            @endif
-        </ul>
+        @endif
+    </ul>
     </div>
 </nav>
