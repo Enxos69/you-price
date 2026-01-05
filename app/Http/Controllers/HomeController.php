ec9e0634382830ca11200fa0d5164de674493701
Enxos69 <enxos@hotmail.it>
2026-01-05 10:50:06 +0100

    fix home page per amministratore e utente

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,18 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $user = Auth::user();
+
+        if ($user->role == 1) {
+            return view('admin.index');
+        }
+
+         if ($user->role == 2) {
+            return app(DashboardController::class)->index();
+        }
+
+        // fallback
+        abort(403);
     }
 }
